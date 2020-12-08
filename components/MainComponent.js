75366672e0774321f6c9389d29fdb5f504b830c0
Emerald Aire <emeraldaire@the-fucking-laptop.local>
2020-12-08 11:49:33 -0800

    Added Drawer Navigator

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import Directory from './DirectoryComponent';
 import { PACKAGES } from '../shared/packages';
 import { JOBS } from '../shared/jobs';
 import { SafeAreaView } from 'react-native';
-import PackageInfo from './PackageInfoComponent';
-import PackageList from './PackageListComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
+import Directory from './DirectoryComponent';
+import PackageInfo from './PackageInfoComponent';
+import PackageList from './PackageListComponent';
+import Home from './HomeComponent';
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -34,23 +36,40 @@ const DirectoryNavigator = createStackNavigator(
 
 );
 
-const AppNavigator = createAppContainer(DirectoryNavigator);
+const HomeNavigator = createStackNavigator(
+    {
+        Home: {screen: Home}
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#b84D05',
+                // backgroundColor: '#F36A0C',
+                
+            },
+            headerTintColor: '#e3e3e3',
+            headerTitleStyle: {
+                color: 'white',
+                fontSize:26,
+                paddingBottom:5
+            }
+        }
+    }
+)
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator },
+        Directory: { screen: DirectoryNavigator }
+    },
+    {
+        drawerBackgroundColor: '#ffdec8'
+    }
+);
+
+const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         packages: PACKAGES,
-    //         jobs: JOBS,
-    //         selectedPackage: null
-    //     };
-    // }
-
-    //Handle package select for Directory ListItem
-    // onPackageSelect(packageId){
-    //     this.setState({selectedPackage: packageId});
-    // }
-
     render() {
         return (
             <View style={{
