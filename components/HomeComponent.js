00c20f8e9fe806eae64f2121900774ace9071ab6
Emerald Aire <emeraldaire@the-fucking-laptop.local>
2020-12-08 18:41:29 -0800

    Added Icons to Detail View

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import { Avatar } from 'react-native-elements';
 
 class Home extends Component {
     
-    static navigationOptions = {
-        title: 'Home'
-    }
+    // static navigationOptions = {
+    //     title: 'Home'
+    // }
 
     render(){
         return (
-            <View>
-                <Text>Home Component</Text>
+            <View style={{alignItems: 'center'}}>
+                <Avatar 
+                    rounded 
+                    size='xlarge' 
+                    marginTop={75} 
+                    source={require('./images/orange-avatar-white-bg.png')}/>
+                <Text style={{fontSize:36, marginTop:20, color: 'black', fontWeight: 'bold'}}>Silo</Text>
             </View>
         );
     }
--- components/PackageInfoComponent.js
+++ components/PackageInfoComponent.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, SafeAreaView } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Text, View, StyleSheet, SafeAreaView, ScrollView } from 'react-native';
+import { Card, Avatar, Icon } from 'react-native-elements';
 import { PACKAGES } from '../shared/packages';
 import { JOBS } from '../shared/jobs';
 
-function RenderPackage({item}){
+function RenderPackageDetails(props){
+    
+    const {item} = props;
+    
     if(item){
         return(
             <SafeAreaView>
+                        {/* <Avatar rounded size='small' source={require('./images/orange-avatar-white-bg.png')}/> */}
                 <Card style={styles.cardContainer}>
-                    <Card.Title style={styles.cardTitle}>
-                        {item.number} {'\n'}
-                        {item.job} {'\n'}
-                        {item.description}
+                    <Card.Title style={{backgroundColor: '#ffc59d', paddingBottom: 10}}>
+                        <View style={styles.viewContainer}>
+                            <Text style={styles.cardTitle}>{item.number}</Text>
+                            <Text style={styles.cardSubtitle}>{item.job}</Text>
+                            <Text style={styles.cardSubtitle}>{item.description}</Text>
+                        </View>
                     </Card.Title>
+                    <View style={styles.cardRow}>
+                        <Icon 
+                            name={props.dispatched ? 'paper-plane-o' : 'paper-plane-o'}
+                            type='font-awesome'
+                            color='#1a7d00'
+                            raised
+                            reverse/>
+                        <Icon 
+                            name={'pencil'}
+                            type='font-awesome'
+                            color='#3662a6'
+                            // color='#0616ae'
+                            // color='#f36a0c'
+                            raised
+                            reverse/>
+                        <Icon 
+                            name={'check'}
+                            type='font-awesome'
+                            color='#e90c25'
+                            // color='#f13f53'
+                            raised
+                            reverse/>
+                    </View>
                     <Text style={styles.cardHeader}>LOCATION</Text>
                     <Text style={styles.cardBody}>{item.location}{'\n'}</Text>
                     <Text style={styles.cardHeader}>STATUS</Text>
@@ -35,7 +64,8 @@ class PackageInfo extends Component {
         super(props);
         this.state = {
             packages: PACKAGES,
-            jobs: JOBS
+            jobs: JOBS,
+            dispatched: false
         }
     }
 
@@ -49,7 +79,11 @@ class PackageInfo extends Component {
         const item = this.state.packages.filter(p => p.id === packageId)[0];
 
         return(
-            <RenderPackage item={item} />
+            <ScrollView>
+                <RenderPackageDetails 
+                    item={item} 
+                    dispatched={this.state.dispatched} />
+            </ScrollView>
         );
     }
 }
@@ -60,26 +94,45 @@ const styles = StyleSheet.create({
     },
 
     cardTitle: {
-        fontSize: 22,
+        fontSize: 28,
         textAlign:"left",
-        padding: 5,
-        backgroundColor: '#e3e3e3'
+        padding: 10,
+        // backgroundColor: '#ffc59d'
+    },
+    cardSubtitle: {
+        fontSize:18,
+        paddingLeft:10,
+        paddingRight:10
     },
     cardHeader: {
         fontSize:18,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        paddingLeft:10,
+        paddingRight:10
         
     },
      cardBody: {
         fontSize: 16,
-        fontStyle: 'italic'
+        fontStyle: 'italic',
+        paddingLeft:10,
+        paddingRight:10
         
     },
-
     cardContainer: {
         marginTop:0,
         marginBottom:0,
-        backgroundColor: 'lightgray'
+        flex: 1
+    },
+    viewContainer: {
+        // backgroundColor: '#ffc59d',
+        flex:2       
+    },
+    cardRow:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        margin: 20
     }
 })
 
